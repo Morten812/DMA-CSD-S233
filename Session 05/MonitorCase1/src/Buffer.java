a505bc531798a5a295e520fe7ac7bdc327013762
Morten812 <10407520@ucn.dk>
2024-03-11 09:22:40 +0100

    Update Buffer.java

--- Session 05/MonitorCase1/src/Buffer.java
+++ Session 05/MonitorCase1/src/Buffer.java
@@ -7,21 +7,23 @@ public class Buffer {
 		buffer = new char[size];
 	}
 
-	public synchronized void Put(char c) {
+	public synchronized void Put(char c) throws InterruptedException {
 		while (count == buffer.length)
-			;
+			wait();
 		System.out.println("Producing " + c + " ...");
 		buffer[in] = c;
 		in = (in + 1) % buffer.length;
 		count++;
+		notifyAll();
 	}
 
-	public synchronized char Get() {
+	public synchronized char Get() throws InterruptedException {
 		while (count == 0)
-			;
+			wait();
 		char c = buffer[out];
 		out = (out + 1) % buffer.length;
 		count--;
+		notifyAll();
 		System.out.println("Consuming " + c + " ...");
 		return c;
 	}
